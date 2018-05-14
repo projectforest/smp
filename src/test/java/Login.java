759ca1c2331a1d2d53e03f1456e8cd49db825244
Henry Lin <projectforest@users.noreply.github.com>
2018-05-14 11:29:45 -0700

    Update Login.java

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -9,10 +9,7 @@ import pageobject.LoginPage;
 public class Login {
     @Test
     public void LMSLogin(WebDriver driver, String branch, String branchAdmin, String branchPassword){
-        //Set Gecko.dirver property to interact to Firefox browser
-        //System.setProperty("webdriver.firefox.marionette","C://Program Files/Mozilla Firefox/geckodriver.exe");
-        // Create a new instance of the firefox driver
-        //WebDriver driver = new FirefoxDriver();
+        
 
         //create object required for this test
         LoginPage aLoginPage = new LoginPage(driver);
@@ -36,14 +33,7 @@ public class Login {
 
         //click login
         aLoginPage.clickLoginButton(driver);
-//        WebElement element = driver.findElement(By.name("j_branch"));
-//        element.sendKeys("Danville");
-//        element = driver.findElement(By.name("j_username"));
-//        element.sendKeys("admin");
-//        element = driver.findElement(By.name("j_password"));
-//        element.sendKeys("hlin1108");
-//        element = driver.findElement(By.id("ext-gen38"));
-//        element.click();
+
         try {
             Thread.sleep(5000);
         }
@@ -52,11 +42,7 @@ public class Login {
         }
         // Read the title of the page and output it into results - need it for debug purpose
         System.out.println("Page title is: " + driver.getTitle());
-        // Verify the title of the page
-        //Assert.assertEquals(driver.getTitle(), "Loan Management System - '" + branch + "' branch");
-        //Assert.assertEquals(driver.getTitle(), "Loan Management System - 'Danville' branch");
-        // close the browser
-        //driver.close();
+        
         try {
             Thread.sleep(5000);
         }
@@ -66,38 +52,3 @@ public class Login {
     }
 }
 
-//public class Login {
-//    public static void main(String[] args) {
-//        //Set Gecko.dirver property to interact to Firefox browser
-//        System.setProperty("webdriver.firefox.marionette","C://Program Files/Mozilla Firefox/geckodriver.exe");
-//        // Create a new instance of the firefox driver
-//        WebDriver driver = new FirefoxDriver();
-//        // And now use this to visit LMS login page
-//        driver.get("http://qa-hitekschool.com/lms/loans/2108");
-//        //driver.get(" http://demo.borland.com/gmopost");
-//        // Maximize window
-//        driver.manage().window().maximize();
-//        // Read the title of the page and put it into results
-//        System.out.print("Page title is: " + driver.getTitle());
-//        WebElement element = driver.findElement(By.name("j_branch"));
-//        element.sendKeys("Danville");
-//        element = driver.findElement(By.name("j_username"));
-//        element.sendKeys("admin");
-//        element = driver.findElement(By.name("j_password"));
-//        element.sendKeys("hlin1108");
-//        element = driver.findElement(By.id("ext-gen38"));
-//        element.click();
-//        try {
-//            Thread.sleep(5000);
-//        }
-//        catch(InterruptedException e){
-//            e.printStackTrace();
-//        }
-//        // Read the title of the page and output it into results - need it for debug purpose
-//        System.out.println("Page title is: " + driver.getTitle());
-//        // Verify the title of the pageAssert.assertEquals(driver.getTitle(), "Loan Management System - 'Danville' branch");
-//        // close the browser
-//        driver.close();
-//
-//    }
-//}
